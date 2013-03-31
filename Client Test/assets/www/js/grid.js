3a106778ead391be1ab014358f93d81a82c6abb7
Fernando Mendoza <elferrus7@gmail.com>
2013-03-31 14:56:39 -0600

    Grid moving
    Now you add images just with the cell number

--- Client Test/assets/www/js/grid.js
+++ Client Test/assets/www/js/grid.js
@@ -28,32 +28,36 @@ function Grid(rows, colums, background){
 	this.cells.attr({fill: "#000", "fill-opacity": 0}); // With this property we can click in the square
 	this.cells.click(function(){
 		if(flag){
-			console.log("Transforming to cell " + this.data("id"));
+			//console.log("Transforming to cell " + this.data("id"));
 			string = "t" + (Math.floor(this.data("id") / rows)) * 50 + "," // Calculando el numero de columnas de la posicion de la celda
 						 + (this.data("id") - (Math.floor(this.data("id") / rows)) * rows ) * 50; // Calculando Numero de fila de la posición de la celda
-			console.log(string);
+			//console.log("Translating " + charid + " al cell "+ this.data("id") + " con " + string);
 			characters[charid].transform(string);
 			flag = false;
 		} else {
-			string = " colum " + (Math.floor(this.data("id") / rows)) + "," // Calculando el numero de columnas de la posicion de la celda
-						 + " Row "+ (this.data("id") - (Math.floor(this.data("id") / rows)) * rows ); // Calculando Numero de fila de la posición de la celda
-			console.log("Helow from " + this.data("id") + string);
+			/*string = " colum " + (Math.floor(this.data("id") / rows)) + "," // Calculando el numero de columnas de la posicion de la celda
+						 + " Row "+ (this.data("id") - (Math.floor(this.data("id") / rows)) * rows ); // Calculando Numero de fila de la posición de la celda*/
+			//console.log("Helow from " + this.data("id") + string);
 			//console.log("Flag " + flag + " charid " + charid);
 		}
 	});
 }
 
-Grid.prototype.addCharacter = function(src,x,y)
+Grid.prototype.addCharacter = function(src,cellid)
 {
-	characters.push(this.paper.image(src,x,y,40,40)
+	characters.push(this.paper.image(src, 5, 5, 40, 40)
 								   .data("id", characters.length)
 								   .click(function(){
 										flag = true;
 										charid = this.data("id");
-										console.log("you clicked " + flag + " " + charid);
+										//console.log("you clicked " + flag + " " + charid);
 									})
 	);
-	//console.log("Character added");
+	string = "t" + (Math.floor(cellid / this.rows)) * 50 + "," // Calculando el numero de columnas de la posicion de la celda
+						 + (cellid - (Math.floor(cellid / this.rows)) * this.rows ) * 50;
+	//console.log("Transforming to " + string);
+	characters[characters.length -1].transform(string);
+	//console.log("Character added in " + 5 + (Math.floor(cellid / this.rows)) * 55 + " , " + 5 + (cellid - (Math.floor(cellid / this.rows)) * this.rows ) * 55);
 	/*this.characters.click(function(){
 		console.log("you clicked " + this.data("id"));
 	});*/
