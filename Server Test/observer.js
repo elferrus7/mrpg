9d25f038e273e364aed505a5f4c7b3e3dfbbfa78
Fernando Mendoza <elferrus7@gmail.com>
2013-04-15 18:22:56 -0500

    Archivo observer

---
+++ Server Test/observer.js
@@ -0,0 +1,63 @@
+/* The interfaze of the Grid Client
+	send the notify to the observer
+*/
+function Grid(observer){
+	var observer = observer;
+}
+
+Grid.prototype.notify = function(){
+	this.observer.warning();
+}
+
+GridClient.prototype.sendJson = function(jason){
+	this.socket.volatile.emit( 'updateGrid',jason);
+}
+
+
+//Receive and send the json to the observer
+function GridClient(id,socket){
+	/*
+	1-Por medio de un post recive el JSON de que existe una actulizacio
+	2-GridClient Notifica por medio de Grid al observer que existe una actualización
+	3-El observer avisa a cada uno de los Grid que existe una actualización
+	4-El Grid obtiene el JSON y se lo pasa a GridClients 
+	5-GridClient notifica al cliente que existe un nuevo JSON
+	6-El mobile Grid obtiene el json
+	7-El mobile Grid actualiza en la interfaz los cambios
+	*/
+	this.socket = socket;
+	this.id = id;
+}
+
+exports.createGridClient = function(id,socket){
+	gcl = new GridClient(id,socket);
+	g = new Grid();
+	gcl.prototype = g.prototype;
+	return gcl;
+}
+
+//Observer Define la interfaz de actualización para los objetos que 
+//deben ser informados de los cambios de un sujeto.
+function Observer(){
+	this.grids = new Array();
+}
+
+exports.createObserver = function(){
+	return new Observer();
+}
+
+Observer.prototype.addGrid = function(grid){
+		this.grids.push(grid);
+}
+
+Observer.prototype.removeGrid = function(id){
+
+}
+
+Observer.prototype.warning = function(socket,jason){
+	for(var i = 0; i < this.grids.length; i++){
+		if(this.grids[i].socket != socket){
+			this.grids[i].sendJson(jason);
+		}
+	}
+}
